9ae2f73e654ba37a0ca9a92b257ed4e6f1dcc944
Allison Miller <anmil.dev@gmail.com>
2023-02-18 19:10:48 -0500

    images and band array

---
+++ shared/bands.js
@@ -0,0 +1,122 @@
+export const BANDS = [
+    {
+        id: 0,
+        name: "Even Flow",
+        image: require("../assets/images/band001.jpg"),
+        genre: "Rock",
+        featured: false,
+        description: "Friday, February 24, 2023 8pm ",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 1,
+        name:"Instrumentals ",
+        image: require("../assets/images/band002.jpg"),
+        genre: "Rock",
+        featured: false,
+        description: "Saturday, February 25, 2023 8pm",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 2,
+        name:"The Horsemen",
+        image: require("../assets/images/band003.jpg"),
+        genre: "Comedy",
+        featured: false,
+        description: "Thursday, March 2, 2023 8pm",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 3,
+        name:"Metal Pedal",
+        image: require("../assets/images/band004.jpg"),
+        genre: "Metal",
+        featured: true,
+        description: "Friday, March 3, 2023 8pm",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 4,
+        name:"Synth Pad",
+        image: require("../assets/images/band005.jpg"),
+        genre: "Pop",
+        featured: true,
+        description: "Saturday March 4, 2023 8pm",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 5,
+        name:"Karikatura",
+        image: require("../assets/images/band006.jpg"),
+        genre: "Punk",
+        featured: true,
+        description: "Friday March 10, 2023 8pm",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 6,
+        name:"SaxTones",
+        image: require("../assets/images/band007.jpg"),
+        genre: "Punk",
+        featured: true,
+        description: "Saturday March 11, 2023 8pm",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 7,
+        name:"The Nobodys",
+        image: require("../assets/images/band008.jpg"),
+        genre: "Rock",
+        featured: true,
+        description: "Thursday March 16, 2023 8pm",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 8,
+        name:"Fireflys",
+        image: require("../assets/images/band009.jpg"),
+        genre: "Rock",
+        featured: true,
+        description: "Friday March 17, 2023 8pm",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 9,
+        name:"Starship",
+        image: require("../assets/images/band010.jpg"),
+        genre: "Metal",
+        featured: true,
+        description: "Saturday March 18, 2023 8pm",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 10,
+        name:"Slap Dat",
+        image: require("../assets/images/band011.jpg"),
+        genre: "Rock",
+        featured: true,
+        description: "Friday March 24, 2023 8pm",
+        tickets: 100,
+        price: 20
+    },
+    {
+        id: 11,
+        name:"Electro Light",
+        image: require("../assets/images/band012.jpg"),
+        genre: "Rock",
+        featured: true,
+        description: "Saturday March 18, 2023 8pm",
+        tickets: 100,
+        price: 20
+    }
+];
