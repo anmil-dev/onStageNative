6d42a9b4717a52dd338f99d8ff8ca545f63640a1
Allison Miller <anmil.dev@gmail.com>
2023-02-27 20:31:07 -0500

    added HomeScreen and Drawer Navigator

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -3,6 +3,29 @@ import EventInfoScreen from './EventInfoScreen';
 import { Platform, View } from 'react-native';
 import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+const screenOptions = {
+    headerTintColor: '#fff',
+    headerStyle: { backgroundColor: '#000000' }
+}
+
+const HomeNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator screenOptions= {screenOptions}>
+            <Stack.Screen
+                name='home'
+                component={HomeScreen}
+                options={{title: 'Home'}}
+            />
+        </Stack.Navigator>
+    )
+}
 
 const EventNavigator = () => {
     const Stack = createStackNavigator();
@@ -10,12 +33,7 @@ const EventNavigator = () => {
     return (
         <Stack.Navigator 
             initialRouteName= 'Directory'
-            screenOptions= {{
-                headerStyle: {
-                    backgroundColor: '#5637DD'
-                },
-                headerTintColor: '#fff'
-            }}
+            screenOptions= { screenOptions }
         >
             <Stack.Screen 
                 name= 'Directory'
@@ -41,7 +59,21 @@ const Main = () => {
                 paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight 
             }}
         >
-            <EventNavigator />
+            <Drawer.Navigator
+                initialRouteName='Home'
+                drawerStyle={{ backgroundColor: '#CEC8FF' }}
+            >
+                <Drawer.Screen 
+                    name= 'Home'
+                    component= {HomeNavigator}
+                    options={{title: 'Home' }}
+                />
+                <Drawer.Screen 
+                    name= 'Events'
+                    component= {EventNavigator}
+                    options={{title: 'Events' }}
+                />
+            </Drawer.Navigator>
         </View>
     );
 };
