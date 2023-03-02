d84181bebc087ac966322f13f52b1c4a41beadbe
Allison Miller <anmil.dev@gmail.com>
2023-03-01 19:14:52 -0500

    carousel here

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import CarouselCards from '../features/Carousel/CarouselCards'
 
 const HomeScreen = () => {
     return (
         <View style={styles.container}>
+            <Text>Carousel here</Text>
             <CarouselCards />
         </View>
     );
